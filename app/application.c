30c2d271540ed4056c9dfeb76167ffe061dd1685
Karel Blavka <karel.blavka@bigclown.com>
2017-08-30 14:31:56 +0200

    Change control mode
    Click change mode, rotete for change value

--- app/application.c
+++ app/application.c
@@ -3,14 +3,19 @@
 
 #define COUNT 70
 
+#define MODE_EFFECT 0
+#define MODE_SPEED 1
+#define MODE_BRIGHTNESS 2
+
 bc_led_t led;
 bc_led_strip_t led_strip;
 uint32_t color;
+int mode = MODE_EFFECT;
 int effect = 0;
 int wait = 45;
-uint8_t brightness = 255;
-int brightness_step = -5;
-bc_scheduler_task_id_t set_brightness_task_id;
+int brightness = 255;
+
+bc_scheduler_task_id_t write_task_id;
 bc_scheduler_task_id_t stroboscope_task_id;
 bc_scheduler_task_id_t snake_task_id;
 
@@ -26,7 +31,7 @@ static const bc_led_strip_buffer_t _led_strip_buffer =
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, void *event_param);
 void encoder_event_handler(bc_module_encoder_event_t event, void *event_param);
-void set_brightness_task(void *param);
+void write_task(void *param);
 void stroboscope_task(void *param);
 void snake_task(void *param);
 
@@ -50,7 +55,7 @@ void application_init(void)
 
     bc_led_strip_effect_rainbow(&led_strip, wait);
 
-    set_brightness_task_id = bc_scheduler_register(set_brightness_task, NULL, BC_TICK_INFINITY);
+    write_task_id = bc_scheduler_register(write_task, NULL, BC_TICK_INFINITY);
     stroboscope_task_id = bc_scheduler_register(stroboscope_task, NULL, BC_TICK_INFINITY);
     snake_task_id = bc_scheduler_register(snake_task, NULL, BC_TICK_INFINITY);
 
@@ -64,58 +69,19 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
 
     if (event == BC_BUTTON_EVENT_PRESS)
     {
-        effect++;
-
-        bc_scheduler_plan_absolute(stroboscope_task_id, BC_TICK_INFINITY);
-        bc_scheduler_plan_absolute(snake_task_id, BC_TICK_INFINITY);
-        bc_led_strip_effect_stop(&led_strip);
-
-        if (effect == 0)
+        if (++mode > 2)
         {
-            bc_led_strip_effect_rainbow(&led_strip, wait);
+            mode = 0;
         }
-        else if (effect == 1)
-        {
-            bc_led_strip_effect_rainbow_cycle(&led_strip, wait);
-        }
-        else if (effect == 2)
-        {
-            color = 0xff000000;
-            bc_led_strip_fill(&led_strip, 0x00000000);
-            bc_led_strip_effect_color_wipe(&led_strip, color, wait);
-        }
-        else if (effect == 3)
-        {
-            bc_led_strip_effect_theater_chase(&led_strip, color, wait);
-        }
-        else if (effect == 4)
-        {
-            bc_led_strip_effect_theater_chase_rainbow(&led_strip, wait);
-        }
-        else if (effect == 5)
-        {
-            bc_scheduler_plan_now(stroboscope_task_id);
-        }
-        else if (effect == 6)
-        {
-            bc_scheduler_plan_now(snake_task_id);
-        }
-        else if (effect == 7)
-        {
-            effect = -1;
-            bc_led_strip_fill(&led_strip, 0x00000000);
-            bc_led_strip_write(&led_strip);
-        }
-
     }
-    else if (event == BC_BUTTON_EVENT_HOLD)
-    {
-        bc_scheduler_plan_now(set_brightness_task_id);
-    }
-    else if (event == BC_BUTTON_EVENT_RELEASE)
-    {
-        bc_scheduler_plan_absolute(set_brightness_task_id, BC_TICK_INFINITY);
-    }
+//    else if (event == BC_BUTTON_EVENT_HOLD)
+//    {
+//        bc_scheduler_plan_now(set_brightness_task_id);
+//    }
+//    else if (event == BC_BUTTON_EVENT_RELEASE)
+//    {
+//        bc_scheduler_plan_absolute(set_brightness_task_id, BC_TICK_INFINITY);
+//    }
 
 }
 
@@ -137,37 +103,122 @@ void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, v
     }
 }
 
+void mode_effect(int increment)
+{
+    effect += increment;
+    if (effect > 7)
+    {
+        effect = 0;
+    }
+    else if (effect < 0)
+    {
+        effect = 7;
+    }
+
+    bc_scheduler_plan_absolute(stroboscope_task_id, BC_TICK_INFINITY);
+    bc_scheduler_plan_absolute(snake_task_id, BC_TICK_INFINITY);
+    bc_led_strip_effect_stop(&led_strip);
+
+    if (effect == 0)
+    {
+        bc_led_strip_effect_rainbow(&led_strip, wait);
+    }
+    else if (effect == 1)
+    {
+        bc_led_strip_effect_rainbow_cycle(&led_strip, wait);
+    }
+    else if (effect == 2)
+    {
+        color = 0xff000000;
+        bc_led_strip_fill(&led_strip, 0x00000000);
+        bc_led_strip_effect_color_wipe(&led_strip, color, wait);
+    }
+    else if (effect == 3)
+    {
+        bc_led_strip_effect_theater_chase(&led_strip, color, wait);
+    }
+    else if (effect == 4)
+    {
+        bc_led_strip_effect_theater_chase_rainbow(&led_strip, wait);
+    }
+    else if (effect == 5)
+    {
+        bc_scheduler_plan_now(stroboscope_task_id);
+    }
+    else if (effect == 6)
+    {
+        bc_led_strip_fill(&led_strip, 0x00000000);
+        bc_scheduler_plan_now(snake_task_id);
+    }
+    else if (effect == 7)
+    {
+        bc_led_strip_fill(&led_strip, 0x00000000);
+        bc_led_strip_write(&led_strip);
+        bc_scheduler_plan_now(write_task_id);
+    }
+}
+
+void mode_speed(int increment)
+{
+    wait += increment * 10;
+
+    if (wait < 10)
+    {
+        wait = 10;
+    }
+    else if (wait > 1000)
+    {
+        wait = 1000;
+    }
+
+    led_strip._effect.wait = (bc_tick_t) wait;
+}
+
+void mode_brightness(int increment)
+{
+    brightness += increment * 5;
+    if (brightness < 0)
+    {
+        brightness = 0;
+    }
+    else if (brightness > 255)
+    {
+        brightness = 255;
+    }
+    bc_led_strip_set_brightness(&led_strip, brightness);
+    bc_scheduler_plan_current_relative(100);
+}
+
 void encoder_event_handler(bc_module_encoder_event_t event, void *event_param)
 {
     (void) event_param;
 
     if (event == BC_MODULE_ENCODER_EVENT_ROTATION)
     {
-        wait += bc_module_encoder_get_increment();
+        int increment = bc_module_encoder_get_increment();
 
-        if (wait < 0)
+        if (mode == MODE_EFFECT)
         {
-            wait = 0;
+            mode_effect(increment > 0 ? 1 : -1);
         }
-
-        led_strip._effect.wait = (bc_tick_t) wait;
+        else if (mode == MODE_SPEED)
+        {
+            mode_speed(increment);
+        }
+        else if (mode == MODE_BRIGHTNESS)
+        {
+            mode_brightness(increment);
+        }
     }
 }
 
-void set_brightness_task(void *param)
+void write_task(void *param)
 {
     (void) param;
-    brightness += brightness_step;
-    if (brightness == 5)
+    if (!bc_led_strip_write(&led_strip))
     {
-        brightness_step = 5;
+        bc_scheduler_plan_current_now();
     }
-    else if (brightness == 255)
-    {
-        brightness_step = -5;
-    }
-    bc_led_strip_set_brightness(&led_strip, brightness);
-    bc_scheduler_plan_current_relative(100);
 }
 
 void stroboscope_task(void *param)
