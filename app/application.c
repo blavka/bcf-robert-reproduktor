0f2aa5485c6f1b9c5b73064fd2d1e36a60c52296
Karel Blavka <karel.blavka@bigclown.com>
2022-06-01 10:23:34 +0200

    Update sdk and change from bc to twr

--- app/application.c
+++ app/application.c
@@ -1,5 +1,5 @@
 #include <application.h>
-#include <bcl.h>
+#include <twr.h>
 
 #define COUNT (72)
 
@@ -7,30 +7,29 @@
 #define MODE_SPEED 1
 #define MODE_BRIGHTNESS 2
 
-bc_led_t led;
-bc_led_strip_t led_strip;
+twr_led_t led;
+twr_led_strip_t led_strip;
 uint32_t color;
 int mode = MODE_EFFECT;
 int effect = 0;
 int wait = 45;
 int brightness = 255;
 
-bc_scheduler_task_id_t write_task_id;
-bc_scheduler_task_id_t stroboscope_task_id;
-bc_scheduler_task_id_t snake_task_id;
+twr_scheduler_task_id_t write_task_id;
+twr_scheduler_task_id_t stroboscope_task_id;
+twr_scheduler_task_id_t snake_task_id;
 
 static uint32_t _dma_buffer[COUNT * 4 * 2]; // count * type * 2
 
-static const bc_led_strip_buffer_t _led_strip_buffer =
-{
-    .type = BC_LED_STRIP_TYPE_RGBW,
-    .count = COUNT,
-    .buffer = _dma_buffer
-};
+static const twr_led_strip_buffer_t _led_strip_buffer =
+    {
+        .type = TWR_LED_STRIP_TYPE_RGBW,
+        .count = COUNT,
+        .buffer = _dma_buffer};
 
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
-void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, void *event_param);
-void encoder_event_handler(bc_module_encoder_event_t event, void *event_param);
+void button_event_handler(twr_button_t *self, twr_button_event_t event, void *event_param);
+void led_strip_event_handler(twr_led_strip_t *self, twr_led_strip_event_t event, void *event_param);
+void encoder_event_handler(twr_module_encoder_event_t event, void *event_param);
 void write_task(void *param);
 void stroboscope_task(void *param);
 void snake_task(void *param);
@@ -38,58 +37,56 @@ void snake_task(void *param);
 void application_init(void)
 {
 
-    bc_led_init(&led, BC_GPIO_LED, false, false);
-    bc_led_set_mode(&led, BC_LED_MODE_ON);
+    twr_led_init(&led, TWR_GPIO_LED, false, false);
+    twr_led_set_mode(&led, TWR_LED_MODE_ON);
 
+    static twr_button_t button;
+    twr_button_init(&button, TWR_GPIO_BUTTON, TWR_GPIO_PULL_DOWN, false);
+    twr_button_set_event_handler(&button, button_event_handler, NULL);
 
-    static bc_button_t button;
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
+    twr_module_encoder_init();
+    twr_module_encoder_set_event_handler(encoder_event_handler, NULL);
 
-    bc_module_encoder_init();
-    bc_module_encoder_set_event_handler(encoder_event_handler, NULL);
+    twr_module_power_init();
+    twr_led_strip_init(&led_strip, twr_module_power_get_led_strip_driver(), &_led_strip_buffer);
+    twr_led_strip_set_event_handler(&led_strip, led_strip_event_handler, NULL);
 
-    bc_module_power_init();
-    bc_led_strip_init(&led_strip, bc_module_power_get_led_strip_driver(), &_led_strip_buffer);
-    bc_led_strip_set_event_handler(&led_strip, led_strip_event_handler, NULL);
+    twr_led_strip_effect_rainbow(&led_strip, wait);
 
-    bc_led_strip_effect_rainbow(&led_strip, wait);
+    write_task_id = twr_scheduler_register(write_task, NULL, TWR_TICK_INFINITY);
+    stroboscope_task_id = twr_scheduler_register(stroboscope_task, NULL, TWR_TICK_INFINITY);
+    snake_task_id = twr_scheduler_register(snake_task, NULL, TWR_TICK_INFINITY);
 
-    write_task_id = bc_scheduler_register(write_task, NULL, BC_TICK_INFINITY);
-    stroboscope_task_id = bc_scheduler_register(stroboscope_task, NULL, BC_TICK_INFINITY);
-    snake_task_id = bc_scheduler_register(snake_task, NULL, BC_TICK_INFINITY);
-
-    bc_led_set_mode(&led, BC_LED_MODE_OFF);
+    twr_led_set_mode(&led, TWR_LED_MODE_OFF);
 }
 
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+void button_event_handler(twr_button_t *self, twr_button_event_t event, void *event_param)
 {
-    (void) self;
-    (void) event_param;
+    (void)self;
+    (void)event_param;
 
-    if (event == BC_BUTTON_EVENT_PRESS)
+    if (event == TWR_BUTTON_EVENT_PRESS)
     {
         if (++mode > 2)
         {
             mode = 0;
         }
     }
-//    else if (event == BC_BUTTON_EVENT_HOLD)
-//    {
-//        bc_scheduler_plan_now(set_brightness_task_id);
-//    }
-//    else if (event == BC_BUTTON_EVENT_RELEASE)
-//    {
-//        bc_scheduler_plan_absolute(set_brightness_task_id, BC_TICK_INFINITY);
-//    }
-
+    //    else if (event == TWR_BUTTON_EVENT_HOLD)
+    //    {
+    //        twr_scheduler_plan_now(set_brightness_task_id);
+    //    }
+    //    else if (event == TWR_BUTTON_EVENT_RELEASE)
+    //    {
+    //        twr_scheduler_plan_absolute(set_brightness_task_id, TWR_TICK_INFINITY);
+    //    }
 }
 
-void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, void *event_param)
+void led_strip_event_handler(twr_led_strip_t *self, twr_led_strip_event_t event, void *event_param)
 {
-    (void) event_param;
+    (void)event_param;
 
-    if (event == BC_LED_STRIP_EVENT_EFFECT_DONE)
+    if (event == TWR_LED_STRIP_EVENT_EFFECT_DONE)
     {
         if (effect == 2)
         {
@@ -98,7 +95,7 @@ void led_strip_event_handler(bc_led_strip_t *self, bc_led_strip_event_t event, v
             {
                 color = 0xff000000;
             }
-            bc_led_strip_effect_color_wipe(self, color, wait);
+            twr_led_strip_effect_color_wipe(self, color, wait);
         }
     }
 }
@@ -115,46 +112,46 @@ void mode_effect(int increment)
         effect = 7;
     }
 
-    bc_scheduler_plan_absolute(stroboscope_task_id, BC_TICK_INFINITY);
-    bc_scheduler_plan_absolute(snake_task_id, BC_TICK_INFINITY);
-    bc_led_strip_effect_stop(&led_strip);
+    twr_scheduler_plan_absolute(stroboscope_task_id, TWR_TICK_INFINITY);
+    twr_scheduler_plan_absolute(snake_task_id, TWR_TICK_INFINITY);
+    twr_led_strip_effect_stop(&led_strip);
 
     if (effect == 0)
     {
-        bc_led_strip_effect_rainbow(&led_strip, wait);
+        twr_led_strip_effect_rainbow(&led_strip, wait);
     }
     else if (effect == 1)
     {
-        bc_led_strip_effect_rainbow_cycle(&led_strip, wait);
+        twr_led_strip_effect_rainbow_cycle(&led_strip, wait);
     }
     else if (effect == 2)
     {
         color = 0xff000000;
-        bc_led_strip_fill(&led_strip, 0x00000000);
-        bc_led_strip_effect_color_wipe(&led_strip, color, wait);
+        twr_led_strip_fill(&led_strip, 0x00000000);
+        twr_led_strip_effect_color_wipe(&led_strip, color, wait);
     }
     else if (effect == 3)
     {
-        bc_led_strip_effect_theater_chase(&led_strip, color, wait);
+        twr_led_strip_effect_theater_chase(&led_strip, color, wait);
     }
     else if (effect == 4)
     {
-        bc_led_strip_effect_theater_chase_rainbow(&led_strip, wait);
+        twr_led_strip_effect_theater_chase_rainbow(&led_strip, wait);
     }
     else if (effect == 5)
     {
-        bc_scheduler_plan_now(stroboscope_task_id);
+        twr_scheduler_plan_now(stroboscope_task_id);
     }
     else if (effect == 6)
     {
-        bc_led_strip_fill(&led_strip, 0x00000000);
-        bc_scheduler_plan_now(snake_task_id);
+        twr_led_strip_fill(&led_strip, 0x00000000);
+        twr_scheduler_plan_now(snake_task_id);
     }
     else if (effect == 7)
     {
-        bc_led_strip_fill(&led_strip, 0x00000000);
-        bc_led_strip_write(&led_strip);
-        bc_scheduler_plan_now(write_task_id);
+        twr_led_strip_fill(&led_strip, 0x00000000);
+        twr_led_strip_write(&led_strip);
+        twr_scheduler_plan_now(write_task_id);
     }
 }
 
@@ -171,7 +168,7 @@ void mode_speed(int increment)
         wait = 1000;
     }
 
-    led_strip._effect.wait = (bc_tick_t) wait;
+    led_strip._effect.wait = (twr_tick_t)wait;
 }
 
 void mode_brightness(int increment)
@@ -185,16 +182,16 @@ void mode_brightness(int increment)
     {
         brightness = 255;
     }
-    bc_led_strip_set_brightness(&led_strip, brightness);
+    twr_led_strip_set_brightness(&led_strip, brightness);
 }
 
-void encoder_event_handler(bc_module_encoder_event_t event, void *event_param)
+void encoder_event_handler(twr_module_encoder_event_t event, void *event_param)
 {
-    (void) event_param;
+    (void)event_param;
 
-    if (event == BC_MODULE_ENCODER_EVENT_ROTATION)
+    if (event == TWR_MODULE_ENCODER_EVENT_ROTATION)
     {
-        int increment = bc_module_encoder_get_increment();
+        int increment = twr_module_encoder_get_increment();
 
         if (mode == MODE_EFFECT)
         {
@@ -213,34 +210,35 @@ void encoder_event_handler(bc_module_encoder_event_t event, void *event_param)
 
 void write_task(void *param)
 {
-    (void) param;
-    if (!bc_led_strip_write(&led_strip))
+    (void)param;
+    if (!twr_led_strip_write(&led_strip))
     {
-        bc_scheduler_plan_current_now();
+        twr_scheduler_plan_current_now();
     }
 }
 
 void stroboscope_task(void *param)
 {
-    (void) param;
-    bc_led_strip_fill(&led_strip, color);
+    (void)param;
+    twr_led_strip_fill(&led_strip, color);
     color = color == 0xff ? 0 : 0xff;
-    bc_led_strip_write(&led_strip);
-    bc_scheduler_plan_current_relative(wait);
+    twr_led_strip_write(&led_strip);
+    twr_scheduler_plan_current_relative(wait);
 }
 
 void snake_task(void *param)
 {
-    (void) param;
+    (void)param;
     const int length = 10;
     static int position = -length;
 
-    for (int i = position; (i < position + length) && i < COUNT; i++) {
+    for (int i = position; (i < position + length) && i < COUNT; i++)
+    {
         if (i < 0)
         {
             continue;
         }
-        bc_led_strip_set_pixel(&led_strip, i, 0x00);
+        twr_led_strip_set_pixel(&led_strip, i, 0x00);
     }
 
     position++;
@@ -251,16 +249,16 @@ void snake_task(void *param)
 
     uint32_t color = 0xff / length;
 
-    for (int i = position; (i < position + length) && i < COUNT; i++) {
+    for (int i = position; (i < position + length) && i < COUNT; i++)
+    {
         if (i < 0)
         {
             continue;
         }
-        bc_led_strip_set_pixel(&led_strip, i, color | 0x20000000);
+        twr_led_strip_set_pixel(&led_strip, i, color | 0x20000000);
         color += 0xff / length;
     }
 
-    bc_led_strip_write(&led_strip);
-    bc_scheduler_plan_current_relative(wait);
+    twr_led_strip_write(&led_strip);
+    twr_scheduler_plan_current_relative(wait);
 }
-
